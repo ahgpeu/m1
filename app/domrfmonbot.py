195bcc543400d2dfbac388df2593e430bb6cc9b4
ahgpeu <andrew@voronezh.name>
2022-02-09 12:31:37 +0300

    removed prints

--- app/domrfmonbot.py
+++ app/domrfmonbot.py
@@ -12,7 +12,6 @@ bad_port_list = []
 with open('config/bot.cfg', 'r') as f:
     api_token = f.read().splitlines()[0]
     f.close()
-print(api_token)
 
 
 def bot_message(botname, message):
@@ -65,7 +64,6 @@ while 1 == 1:
     with open('config/ports.cfg', 'r') as f:
         ListAll = [line.rstrip('\n').split(';') for line in f]
         f.close()
-    print(ListAll)
     for member in ListAll:
         count = 0
         res1 = knock_function(member, count)
@@ -74,6 +72,7 @@ while 1 == 1:
             bot_message(chat_name, str(member[2]) + ' offline')
         else:
             print('result of test: ' + str(res1))
+            good_port_list.append(member)
 
     with open('config/hosts.cfg', 'r') as f:
         addres_list = f.read().splitlines()
@@ -83,11 +82,21 @@ while 1 == 1:
         remains = ssl_expiry_datetime(member, 443)
         if remains != '0':
             if int(str((remains - datetime.datetime.now()).days)) < 30:
-                bot_message(chat_name, str(member) + ' истекает сертификат!')
                 good_list.append([member, (remains - datetime.datetime.now()).days])
+                bot_message(chat_name, str(member) + ' истекает сертификат!')
+            else:
+                good_list.append(member)
         else:
             bad_list.append(member)
             bot_message(chat_name, str(member) + ' НЕДОСТУПЕН!')
-    #    print(good_port_list)
-    #    print(bad_port_list)
-    time.sleep(300)
+    print(good_list)
+    print(bad_list)
+    print(good_port_list)
+    print(bad_port_list)
+    if len(bad_list) > 0 or len(bad_port_list):
+        for member in bad_list:
+            bot_message(chat_name, str(member) + ' НЕДОСТУПЕН')
+        for member in bad_port_list:
+            bot_message(chat_name, str(member[2]) + ' offline')
+        time.sleep(300)
+    time.sleep(5)
